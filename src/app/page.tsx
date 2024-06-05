9f80a2f2ada86546ad4ebe887af5b2cc2f2dacbf
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-05 15:45:47 +0600

    refactor : separate navbar in different component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { BookmarkIcon, SunIcon } from "lucide-react";
+import Navbar from "@/components/partials/navbar";
 
 export default function page() {
   return (
     <div>
-      <nav className="p-4 fixed top-0 w-full border-b flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <p className="font-bold text-2xl">W3Jobs</p>
-          <form className="flex items-center gap-2">
-            <Input type="text" placeholder="react job, angular job" />
-            <Button>Search</Button>
-          </form>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Button size={'icon'} className="rounded-full" variant={'ghost'}>
-            <BookmarkIcon />
-          </Button>
-          <ModeToggle />
-          <Avatar className="ml-4">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </div>
-      </nav>
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/partials/navbar.tsx
@@ -0,0 +1,30 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { BookmarkIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "../mode-toggle";
+
+export default function Navbar() {
+    return (
+        <nav className="p-4 fixed top-0 w-full border-b flex justify-between items-center">
+            <div className="flex items-center gap-4">
+                <p className="font-bold text-2xl">W3Jobs</p>
+                <form className="flex items-center gap-2">
+                    <Input type="text" placeholder="react job, angular job" />
+                    <Button>Search</Button>
+                </form>
+            </div>
+
+            <div className="flex items-center gap-2">
+                <Button size={'icon'} className="rounded-full" variant={'ghost'}>
+                    <BookmarkIcon />
+                </Button>
+                <ModeToggle />
+                <Avatar className="ml-4">
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+        </nav>
+    )
+}
