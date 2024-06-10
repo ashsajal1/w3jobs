349dbf70e8fb1547192e1862c973d034366a1ed5
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-10 14:30:34 +0600

    style: design header of job application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,49 @@ import JobCard from "@/components/partials/job-card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Bookmark, EyeIcon } from "lucide-react";
+import { Bookmark, EyeIcon, ShareIcon } from "lucide-react";
 
 export default function page() {
-  return (
-    <div className="flex w-full justify-between">
-      <section className="w-full border">
-        <div className="w-full p-4">
-          12 results found
+    return (
+        <div className="flex w-full justify-between">
+            <section className="w-full border">
+                <div className="w-full p-4">
+                    12 results found
+                </div>
+
+                <div className="flex flex-col">
+                    <JobCard />
+                    <JobCard />
+                    <JobCard />
+                    <JobCard />
+                    <JobCard />
+                </div>
+            </section>
+
+            <section className="w-full border hidden md:flex md:flex-col p-4">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <p>Company name</p>
+                    </div>
+                    <div>
+                        <Bookmark />
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="font-bold text-2xl mt-4">React Developer</h2>
+                    <div className="flex items-center gap-2 text-[12px]">
+                        <span>United States</span>
+                        <span>2 days ago</span>
+                        <span>12 Applicants</span>
+                    </div>
+                </div>
+            </section>
+
         </div>
-
-        <div className="flex flex-col">
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-        </div>
-      </section>
-
-      <section className="w-full border hidden md:flex">
-        <p>Company name</p>
-      </section>
-
-    </div>
-  )
+    )
 }
