fd401b812f0d9bd4b12305fe66e7400b5e8a44ce
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-10 15:44:22 +0600

    feat: add job desc and company details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import JobCard from "@/components/partials/job-card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Bookmark, Brain, BriefcaseBusinessIcon, Building, EyeIcon, ShareIcon } from "lucide-react";
+import { Bookmark, Brain, BriefcaseBusinessIcon, Building, Ellipsis, EyeIcon, MousePointerClick, ShareIcon } from "lucide-react";
 
 export default function page() {
     return (
@@ -31,7 +31,9 @@ export default function page() {
                         <p>Company name</p>
                     </div>
                     <div>
-                        <Bookmark />
+                        <Button variant={'ghost'} size={'icon'}>
+                            <Ellipsis />
+                        </Button>
                     </div>
                 </div>
 
@@ -68,6 +70,55 @@ export default function page() {
                             <p>NextJs</p>
                         </div>
                     </div>
+                </div>
+
+                <div className="flex items-center gap-2 mt-4">
+                    <Button size={'sm'}>
+                        <MousePointerClick className="size-4 mr-1" />
+                        <span>Apply</span>
+                    </Button>
+                    <Button size={'sm'} variant={'outline'}>
+                        <Bookmark className="size-4 mr-1" />
+                        <span>Save</span>
+                    </Button>
+                </div>
+
+                <div className="mt-4">
+                    <h3 className="text-xl font-bold">About the job</h3>
+
+                    <div>
+                        <div className="mt-4">
+                            <h4 className="font-bold">Role Description</h4>
+                            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione pariatur hic, eveniet animi adipisci molestias ad quos aut ex quaerat fugiat ipsa sequi accusamus? Veritatis eaque adipisci consequatur veniam eos!</p>
+                        </div>
+                        <div className="mt-4">
+                            <h4 className="font-bold">Qualification</h4>
+                            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione pariatur hic, eveniet animi adipisci molestias ad quos aut ex quaerat fugiat ipsa sequi accusamus? Veritatis eaque adipisci consequatur veniam eos!</p>
+                        </div>
+                        <div className="mt-4">
+                            <h4 className="font-bold">Your work</h4>
+                            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione pariatur hic, eveniet animi adipisci molestias ad quos aut ex quaerat fugiat ipsa sequi accusamus? Veritatis eaque adipisci consequatur veniam eos!</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="mt-3 p-4 border rounded">
+                    <h3 className="font-bold text-xl">About the company</h3>
+
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-2">
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <p>Company name</p>
+                        </div>
+
+                        <Button size={'sm'} variant={'outline'}>Follow</Button>
+                    </div>
+
+                    <p className="text-sm mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quae sapiente doloribus numquam vero natus recusandae consequuntur accusamus, dignissimos repellat laudantium eum asperiores inventore velit. Adipisci asperiores blanditiis numquam impedit.</p>
                 </div>
             </section>
 
