56fd7670b431738701560215cf7eb7886d794f75
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-11 17:08:08 +0600

    feat: create create route

---
+++ src/app/api/jobs/route.ts
@@ -0,0 +1,44 @@
+import { type NextRequest } from "next/server";
+import prisma from "../../../../prisma/client";
+
+export async function GET() {
+  const jobs = await prisma.job.findMany();
+  return Response.json(jobs);
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const {
+    title,
+    description,
+    authorId,
+    country,
+    companyName,
+    companyDetails,
+    minSalary,
+    maxSalary,
+    totalApplicants,
+    skills,
+    url,
+    employeesQuantity,
+    jobType,
+  } = data;
+  const newJob = await prisma.job.create({
+    data: {
+      title,
+      description,
+      authorId,
+      country,
+      companyName,
+      companyDetails,
+      minSalary,
+      maxSalary,
+      totalApplicants,
+      skills,
+      url,
+      employeesQuantity,
+      jobType,
+    },
+  });
+  return Response.json(newJob);
+}
