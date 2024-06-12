faef4af5e87a2a349e7f943e0b6e304911f6f0f8
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-12 17:49:26 +0600

    feat: wrap field inside form

--- src/app/jobs/create/page.tsx
+++ src/app/jobs/create/page.tsx
@@ -7,97 +7,103 @@ import { Textarea } from "@/components/ui/textarea";
 export default function page() {
     const experiences = ['Enter level', 'Mid level', 'Senior']
     const workType = ['Remote', 'Onsite', 'Hybrid']
+    const handleForm = async (formData: FormData) => {
+        "use server"
+        // console.log(formData.get('title'))
+    }
     return (
         <div className="p-4">
-            <h1 className="text-4xl font-bold mb-4">Create a job post</h1>
+            <form action={handleForm}>
+                <h1 className="text-4xl font-bold mb-4">Create a job post</h1>
 
-            <div>
-                <Label>Job title</Label>
-                <Input placeholder='Title eg. Frontend Dev, Fullstack web dev, Data Engineer' />
-            </div>
+                <div>
+                    <Label>Job title</Label>
+                    <Input required name='title' placeholder='Title eg. Frontend Dev, Fullstack web dev, Data Engineer' />
+                </div>
 
-            <section className="md:flex md:items-center md:justify-between md:gap-6">
-                <section className="flex md:flex-row flex-col items-center gap-4">
-                    <div className="flex w-full items-center gap-4">
-                        <div className="mt-4 w-full">
-                            <Label>Select Country</Label>
-                            <Select>
-                                <SelectTrigger className="w-full md:w-[180px]">
-                                    <SelectValue placeholder="Country" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectGroup>
-                                        <SelectLabel>Country</SelectLabel>
-                                        <SelectItem value="United States">United States</SelectItem>
-                                        <SelectItem value="Bangladesh">Bangladesh</SelectItem>
+                <section className="md:flex md:items-center md:justify-between md:gap-6">
+                    <section className="flex md:flex-row flex-col items-center gap-4">
+                        <div className="flex w-full items-center gap-4">
+                            <div className="mt-4 w-full">
+                                <Label>Select Country</Label>
+                                <Select required name="country">
+                                    <SelectTrigger className="w-full md:w-[180px]">
+                                        <SelectValue placeholder="Country" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            <SelectLabel>Country</SelectLabel>
+                                            <SelectItem value="United States">United States</SelectItem>
+                                            <SelectItem value="Bangladesh">Bangladesh</SelectItem>
 
-                                    </SelectGroup>
-                                </SelectContent>
-                            </Select>
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+
+                            <div className="mt-4 w-full">
+                                <Label>Select Experience</Label>
+                                <Select required name='experience'>
+                                    <SelectTrigger className="w-full md:w-[180px]">
+                                        <SelectValue placeholder="Experience" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            <SelectLabel>Experience</SelectLabel>
+                                            {
+                                                experiences.map((experience: string) => (
+                                                    <SelectItem key={experience} value={experience}>{experience}</SelectItem>
+
+                                                ))
+                                            }
+
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                            </div>
                         </div>
 
                         <div className="mt-4 w-full">
-                            <Label>Select Experience</Label>
-                            <Select>
-                                <SelectTrigger className="w-full md:w-[180px]">
-                                    <SelectValue placeholder="Experience" />
+                            <Label>Select Work Type</Label>
+                            <Select required name='jobType'>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Remote" />
                                 </SelectTrigger>
-                                <SelectContent>
+                                <SelectContent className="w-full">
                                     <SelectGroup>
-                                        <SelectLabel>Experience</SelectLabel>
+                                        <SelectLabel>Work type</SelectLabel>
                                         {
-                                            experiences.map((experience: string) => (
-                                                <SelectItem key={experience} value={experience}>{experience}</SelectItem>
+                                            workType.map((wt: string) => (
+                                                <SelectItem key={wt} value={wt}>{wt}</SelectItem>
 
                                             ))
                                         }
-
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
                         </div>
-                    </div>
 
-                    <div className="mt-4 w-full">
-                        <Label>Select Work Type</Label>
-                        <Select>
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Remote" />
-                            </SelectTrigger>
-                            <SelectContent className="w-full">
-                                <SelectGroup>
-                                    <SelectLabel>Work type</SelectLabel>
-                                    {
-                                        workType.map((wt: string) => (
-                                            <SelectItem key={wt} value={wt}>{wt}</SelectItem>
+                    </section>
 
-                                        ))
-                                    }
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </div>
-
+                    <section className="flex items-center gap-4 md:w-auto w-full">
+                        <div className="mt-4 w-full md:w-auto">
+                            <Label>Enter minimum salary in USD</Label>
+                            <Input name='minSalary' type="number" placeholder="Eg. 40000, 80000" />
+                        </div>
+                        <div className="mt-4 w-full md:w-auto">
+                            <Label>Enter maximum salary in USD</Label>
+                            <Input name='maxSalary' type="number" placeholder="Eg. 100000, 260000" />
+                        </div>
+                    </section>
                 </section>
 
-                <section className="flex items-center gap-4 md:w-auto w-full">
-                    <div className="mt-4 w-full md:w-auto">
-                        <Label>Enter minimum salary in USD</Label>
-                        <Input type="number" placeholder="Eg. 40000, 80000" />
-                    </div>
-                    <div className="mt-4 w-full md:w-auto">
-                        <Label>Enter maximum salary in USD</Label>
-                        <Input type="number" placeholder="Eg. 100000, 260000" />
-                    </div>
-                </section>
-            </section>
+                <div className="mt-4">
+                    <Label>Job description</Label>
+                    <Textarea required name='description' placeholder="Job description" />
+                </div>
 
-            <div className="mt-4">
-                <Label>Job description</Label>
-                <Textarea placeholder="Job description" />
-            </div>
-
-            <Button className="mt-4 w-full">Submit</Button>
+                <Button className="mt-4 w-full">Submit</Button>
+            </form>
         </div>
     )
 }
