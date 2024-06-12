3738c1939b2e1e6a19e6237af649313c8b7d0081
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-12 15:15:54 +0600

    fix: give correct path

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -2,8 +2,8 @@ import JobCard from "@/components/partials/job-card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Bookmark, Brain, BriefcaseBusinessIcon, Building, Ellipsis, MousePointerClick } from "lucide-react";
-import prisma from "../../lib/client";
 import { timeAgo } from "@/lib/utils";
+import prisma from "@/lib/prisma";
 
 
 const fetchJobs = async () => {
