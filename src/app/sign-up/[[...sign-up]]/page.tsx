07df9e8f796a2ca971750d9ac91d536781dda8d9
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-13 13:15:45 +0600

    style: design layout of singin singup and profile page

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+    return <div className='grid place-items-center p-6'>
+        <SignUp />
+    </div>;
 }
