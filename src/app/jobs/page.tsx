011e10ff2f0f64ecc725b488d4a2d77bef930433
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-14 17:02:12 +0600

    feat: enhance dynamic seo of jobs page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -5,6 +5,39 @@ import { Bookmark, Brain, BriefcaseBusinessIcon, Building, Ellipsis, MousePointe
 import { timeAgo } from "@/lib/utils";
 import prisma from "@/lib/prisma";
 import { Badge } from "@/components/ui/badge"
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string }
+}
+
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    const jobs = await fetchJobs()
+    let selectedJob: typeof jobs[0] | null;
+    const id = searchParams.id;
+    if (searchParams.id) {
+        selectedJob = jobs.find(job => job.id === parseInt(id)) || jobs[0];
+    } else {
+        selectedJob = null
+    }
+
+    if (selectedJob) {
+        return {
+            title: selectedJob.title + " at " + selectedJob.companyName,
+            description: selectedJob.description
+        }
+    }
+
+    return {
+        title: "Discover Your Next Career Move on w3jobs",
+        description: "Unlock endless opportunities and take your career to new heights with w3jobs. As the leading platform for professionals, we connect you with top employers and the perfect job matches across various industries. Start your journey to success today.",
+    }
+
+}
 
 const fetchJobs = async () => {
     const jobs = await prisma.job.findMany();
