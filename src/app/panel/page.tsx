3c239894b2ca285535bd6e13b5e3eb890a0f7e05
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-27 08:13:16 +0600

    feat: create panel page

---
+++ src/app/panel/page.tsx
@@ -0,0 +1,24 @@
+import JobCard from "@/components/partials/job-card";
+import prisma from "@/lib/prisma";
+
+const fetchJobs = async () => {
+    const jobs = await prisma.job.findMany();
+    return jobs;
+};
+
+export default async function page() {
+    const jobs = await fetchJobs();
+    return (
+        <div>
+            <h1>Welcome to panel!</h1>
+            <div>
+                <h2>List of jobs</h2>
+                <div>
+                    {jobs.map(job => (
+                        <JobCard key={job.id} job={job} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
