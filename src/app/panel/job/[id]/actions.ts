da2138757597a38687e74fff253cce243ece121d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-27 11:17:44 +0600

    feat: add action for edit all fields

--- src/app/panel/job/[id]/actions.ts
+++ src/app/panel/job/[id]/actions.ts
@@ -11,3 +11,59 @@ export const getJob = async (id: number) => {
 
   return job;
 };
+
+export const updateCompanyName = async (id: number, companyName: string) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { companyName }
+    });
+};
+
+export const updateTitle = async (id: number, title: string) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { title }
+    });
+};
+
+export const updateCountry = async (id: number, country: string) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { country }
+    });
+};
+
+export const updateSalary = async (id: number, minSalary: number, maxSalary: number) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { minSalary, maxSalary }
+    });
+};
+
+export const updateEmployeesQuantity = async (id: number, employeesQuantity: string) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { employeesQuantity: parseInt(employeesQuantity) }
+    });
+};
+
+export const updateSkills = async (id: number, skills: string) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { skills }
+    });
+};
+
+export const updateDescription = async (id: number, description: string) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { description }
+    });
+};
+
+export const updateCompanyDetails = async (id: number, companyDetails: string) => {
+    return await prisma.job.update({
+        where: { id },
+        data: { companyDetails }
+    });
+};
--- src/app/panel/job/[id]/job-editable-body.tsx
+++ src/app/panel/job/[id]/job-editable-body.tsx
@@ -1,37 +1,144 @@
-import React from 'react';
-import JobCard from "@/app/jobs/job-card";
+import React, { useState } from 'react';
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Bookmark, Brain, BriefcaseBusinessIcon, Building, Ellipsis, MousePointerClick } from "lucide-react";
 import { timeAgo } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
 import { Job } from "@/lib/types";
+import { Input } from '@/components/ui/input';
+
+import {
+    updateCompanyName,
+    updateTitle,
+    updateCountry,
+    updateSalary,
+    updateEmployeesQuantity,
+    updateSkills,
+    updateDescription,
+    updateCompanyDetails
+} from './actions';
+import { Textarea } from '@/components/ui/textarea';
 
 type JobPageBodyProps = {
     selectedJob: Job;
 };
+
 const JobEditableBody: React.FC<JobPageBodyProps> = ({ selectedJob }) => {
+    const [isEditingCompanyName, setIsEditingCompanyName] = useState(false);
+    const [isEditingTitle, setIsEditingTitle] = useState(false);
+    const [isEditingCountry, setIsEditingCountry] = useState(false);
+    const [isEditingSalary, setIsEditingSalary] = useState(false);
+    const [isEditingEmployeesQuantity, setIsEditingEmployeesQuantity] = useState(false);
+    const [isEditingSkills, setIsEditingSkills] = useState(false);
+    const [isEditingDescription, setIsEditingDescription] = useState(false);
+    const [isEditingCompanyDetails, setIsEditingCompanyDetails] = useState(false);
+
+    const [companyName, setCompanyName] = useState(selectedJob.companyName);
+    const [title, setTitle] = useState(selectedJob.title);
+    const [country, setCountry] = useState(selectedJob.country);
+    const [minSalary, setMinSalary] = useState(selectedJob.minSalary);
+    const [maxSalary, setMaxSalary] = useState(selectedJob.maxSalary);
+    const [employeesQuantity, setEmployeesQuantity] = useState(selectedJob.employeesQuantity);
+    const [skills, setSkills] = useState(selectedJob.skills);
+    const [description, setDescription] = useState(selectedJob.description);
+    const [companyDetails, setCompanyDetails] = useState(selectedJob.companyDetails);
+
+    const handleBlur = async (field: string) => {
+        switch (field) {
+            case 'companyName':
+                await updateCompanyName(selectedJob.id, companyName);
+                setIsEditingCompanyName(false);
+                break;
+            case 'title':
+                await updateTitle(selectedJob.id, title);
+                setIsEditingTitle(false);
+                break;
+            case 'country':
+                await updateCountry(selectedJob.id, country);
+                setIsEditingCountry(false);
+                break;
+            case 'salary':
+                await updateSalary(selectedJob.id, minSalary, maxSalary);
+                setIsEditingSalary(false);
+                break;
+            case 'employeesQuantity':
+                await updateEmployeesQuantity(selectedJob.id, employeesQuantity || 0);
+                setIsEditingEmployeesQuantity(false);
+                break;
+            case 'skills':
+                await updateSkills(selectedJob.id, skills);
+                setIsEditingSkills(false);
+                break;
+            case 'description':
+                await updateDescription(selectedJob.id, description);
+                setIsEditingDescription(false);
+                break;
+            case 'companyDetails':
+                await updateCompanyDetails(selectedJob.id, companyDetails || ' Default');
+                setIsEditingCompanyDetails(false);
+                break;
+            default:
+                break;
+        }
+    };
+
     return (
-        <section className={`w-full border p-4`}>
+        <section className="w-full border p-4">
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-2">
                     <Avatar>
                         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                        <AvatarFallback>{selectedJob.companyName.charAt(0)}</AvatarFallback>
+                        <AvatarFallback>{companyName.charAt(0)}</AvatarFallback>
                     </Avatar>
-                    <p>{selectedJob.companyName}</p>
+                    <div className='flex gap-2 items-center'>
+                        {isEditingCompanyName ? (
+                            <Input
+                                type="text"
+                                value={companyName}
+                                onChange={(e) => setCompanyName(e.target.value)}
+                                onBlur={() => handleBlur('companyName')}
+                            />
+                        ) : (
+                            <p>{companyName}</p>
+                        )}
+                        <Button variant="outline" size="icon" onClick={() => setIsEditingCompanyName(!isEditingCompanyName)}>Edit</Button>
+                    </div>
                 </div>
                 <div>
-                    <Button variant={'ghost'} size={'icon'}>
+                    <Button variant="ghost" size="icon">
                         <Ellipsis />
                     </Button>
                 </div>
             </div>
 
             <div>
-                <h2 className="font-bold text-2xl mt-4">{selectedJob.title}</h2>
+                <h2 className="font-bold text-2xl mt-4">
+                    {isEditingTitle ? (
+                        <Input
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            onBlur={() => handleBlur('title')}
+                        />
+                    ) : (
+                        title
+                    )}
+                </h2>
+                <Button variant="outline" size="icon" onClick={() => setIsEditingTitle(!isEditingTitle)}>Edit</Button>
                 <div className="flex items-center gap-2 text-[12px]">
-                    <span>{selectedJob.country}</span>
+                    <span>
+                        {isEditingCountry ? (
+                            <Input
+                                type="text"
+                                value={country}
+                                onChange={(e) => setCountry(e.target.value)}
+                                onBlur={() => handleBlur('country')}
+                            />
+                        ) : (
+                            country
+                        )}
+                    </span>
+                    <Button variant="outline" size="icon" onClick={() => setIsEditingCountry(!isEditingCountry)}>Edit</Button>
                     <span>{timeAgo(selectedJob.createdAt)}</span>
                     <span>{selectedJob.totalApplicants} Applicants</span>
                 </div>
@@ -41,18 +148,47 @@ const JobEditableBody: React.FC<JobPageBodyProps> = ({ selectedJob }) => {
                 <div className="flex items-center gap-2">
                     <BriefcaseBusinessIcon />
                     <div className="flex items-center gap-2">
-                        {selectedJob.minSalary && <p>
-                            <span>${selectedJob.minSalary}</span>
-                            <span> - </span>
-                            <span>${selectedJob.maxSalary}</span>
-                        </p>}
+                        {isEditingSalary ? (
+                            <>
+                                <Input
+                                    type="number"
+                                    value={minSalary}
+                                    onChange={(e) => setMinSalary(Number(e.target.value))}
+                                    onBlur={() => handleBlur('salary')}
+                                />
+                                <span> - </span>
+                                <Input
+                                    type="number"
+                                    value={maxSalary}
+                                    onChange={(e) => setMaxSalary(Number(e.target.value))}
+                                    onBlur={() => handleBlur('salary')}
+                                />
+                            </>
+                        ) : (
+                            <p>
+                                <span>${minSalary}</span>
+                                <span> - </span>
+                                <span>${maxSalary}</span>
+                            </p>
+                        )}
+                        <Button variant="outline" size="icon" onClick={() => setIsEditingSalary(!isEditingSalary)}>Edit</Button>
                         <Badge>{selectedJob.jobType}</Badge>
                     </div>
                 </div>
                 <div className="flex items-center gap-2">
                     <Building />
                     <div className="flex items-center gap-2">
-                        <p>{selectedJob.employeesQuantity || '0-10'} employees</p>
+                        {isEditingEmployeesQuantity ? (
+                            <Input
+                                type="text"
+                                value={employeesQuantity || 0}
+                                onChange={(e) => setEmployeesQuantity(parseInt(e.target.value))}
+                                onBlur={() => handleBlur('employeesQuantity')}
+                            />
+                        ) : (
+                            <p>{employeesQuantity || '0-10'} employees</p>
+                        )}
+                        <Button variant="outline" size="icon" onClick={() => setIsEditingEmployeesQuantity(!isEditingEmployeesQuantity)}>Edit</Button>
                     </div>
                 </div>
                 <div className="flex items-center gap-2">
@@ -61,20 +197,30 @@ const JobEditableBody: React.FC<JobPageBodyProps> = ({ selectedJob }) => {
                     </div>
                     <div className="flex items-center gap-2">
                         <div className="flex-wrap flex gap-2">
-                            {selectedJob.skills.split(" ").map(skill => (
-                                <Badge variant={'secondary'} key={skill}>{skill}</Badge>
-                            ))}
+                            {isEditingSkills ? (
+                                <Input
+                                    type="text"
+                                    value={skills}
+                                    onChange={(e) => setSkills(e.target.value)}
+                                    onBlur={() => handleBlur('skills')}
+                                />
+                            ) : (
+                                skills.split(" ").map(skill => (
+                                    <Badge variant="secondary" key={skill}>{skill}</Badge>
+                                ))
+                            )}
                         </div>
+                        <Button variant="outline" size="icon" onClick={() => setIsEditingSkills(!isEditingSkills)}>Edit</Button>
                     </div>
                 </div>
             </div>
 
             <div className="flex items-center gap-2 mt-4">
-                <Button size={'sm'}>
+                <Button size="sm">
                     <MousePointerClick className="size-4 mr-1" />
                     <span>Apply</span>
                 </Button>
-                <Button size={'sm'} variant={'outline'}>
+                <Button size="sm" variant="outline">
                     <Bookmark className="size-4 mr-1" />
                     <span>Save</span>
                 </Button>
@@ -85,7 +231,16 @@ const JobEditableBody: React.FC<JobPageBodyProps> = ({ selectedJob }) => {
                 <div>
                     <div className="mt-4">
                         <h4 className="font-bold">Description</h4>
-                        <p className="text-sm">{selectedJob.description}</p>
+                        {isEditingDescription ? (
+                            <Textarea
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                onBlur={() => handleBlur('description')}
+                            />
+                        ) : (
+                            <p className="text-sm">{description}</p>
+                        )}
+                        <Button variant="outline" size="icon" onClick={() => setIsEditingDescription(!isEditingDescription)}>Edit</Button>
                     </div>
                 </div>
             </div>
@@ -97,14 +252,24 @@ const JobEditableBody: React.FC<JobPageBodyProps> = ({ selectedJob }) => {
                     <div className="flex items-center gap-2">
                         <Avatar>
                             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                            <AvatarFallback>{selectedJob.companyName} Profile Photo</AvatarFallback>
+                            <AvatarFallback>{companyName} Profile Photo</AvatarFallback>
                         </Avatar>
-                        <p>{selectedJob.companyName}</p>
+                        {isEditingCompanyDetails ? (
+                            <Input
+                                type="text"
+                                value={companyDetails || ' '}
+                                onChange={(e) => setCompanyDetails(e.target.value)}
+                                onBlur={() => handleBlur('companyDetails')}
+                            />
+                        ) : (
+                            <p>{companyDetails}</p>
+                        )}
+                        <Button variant="outline" size="icon" onClick={() => setIsEditingCompanyDetails(!isEditingCompanyDetails)}>Edit</Button>
                     </div>
-                    <Button size={'sm'} variant={'outline'}>Follow</Button>
+                    <Button size="sm" variant="outline">Follow</Button>
                 </div>
 
-                <p className="text-sm mt-4">{selectedJob.companyDetails}</p>
+                <p className="text-sm mt-4">{companyDetails}</p>
             </div>
         </section>
     );
