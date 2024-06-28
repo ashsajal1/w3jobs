747960edc1b535b3d2a63f1849c5e91cd585e6d6
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-28 06:31:25 +0600

    refactor: import Job type

--- src/app/jobs/jobsPageBody.tsx
+++ src/app/jobs/jobsPageBody.tsx
@@ -5,25 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Bookmark, Brain, BriefcaseBusinessIcon, Building, Ellipsis, MousePointerClick } from "lucide-react";
 import { timeAgo } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
-
-interface Job {
-    id: number;
-    title: string;
-    description: string;
-    authorId: string;
-    country: string;
-    companyName: string;
-    companyDetails: string | null;
-    minSalary: number;
-    maxSalary: number;
-    totalApplicants: number;
-    skills: string;
-    url: string | null;
-    employeesQuantity: number | null;
-    jobType: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
+import { Job } from "@/lib/types"
 
 type JobPageBodyProps = {
     jobs: Job[];
@@ -47,101 +29,101 @@ const JobPageBody: React.FC<JobPageBodyProps> = ({ jobs, selectedJob, id }) => {
             </section>
 
             {selectedJob && (
-            <section key={selectedJob.id} className={`w-full border p-4 ${id ? '' : 'hidden md:flex md:flex-col'}`}>
-                <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                        <Avatar>
-                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                            <AvatarFallback>{selectedJob.companyName.charAt(0)}</AvatarFallback>
-                        </Avatar>
-                        <p>{selectedJob.companyName}</p>
-                    </div>
-                    <div>
-                        <Button variant={'ghost'} size={'icon'}>
-                            <Ellipsis />
-                        </Button>
-                    </div>
-                </div>
-
-                <div>
-                    <h2 className="font-bold text-2xl mt-4">{selectedJob.title}</h2>
-                    <div className="flex items-center gap-2 text-[12px]">
-                        <span>{selectedJob.country}</span>
-                        <span>{timeAgo(selectedJob.createdAt)}</span>
-                        <span>{selectedJob.totalApplicants} Applicants</span>
-                    </div>
-                </div>
-
-                <div className="mt-4 flex flex-col gap-2">
-                    <div className="flex items-center gap-2">
-                        <BriefcaseBusinessIcon />
-                        <div className="flex items-center gap-2">
-                            {selectedJob.minSalary && <p>
-                                <span>${selectedJob.minSalary}</span>
-                                <span> - </span>
-                                <span>${selectedJob.maxSalary}</span>
-                            </p>}
-                            <Badge>{selectedJob.jobType}</Badge>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Building />
-                        <div className="flex items-center gap-2">
-                            <p>{selectedJob.employeesQuantity || '0-10'} employees</p>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <div>
-                            <Brain />
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <div className="flex-wrap flex gap-2">
-                                {selectedJob.skills.split(" ").map(skill => (
-                                    <Badge variant={'secondary'} key={skill}>{skill}</Badge>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex items-center gap-2 mt-4">
-                    <Button size={'sm'}>
-                        <MousePointerClick className="size-4 mr-1" />
-                        <span>Apply</span>
-                    </Button>
-                    <Button size={'sm'} variant={'outline'}>
-                        <Bookmark className="size-4 mr-1" />
-                        <span>Save</span>
-                    </Button>
-                </div>
-
-                <div className="mt-4">
-                    <h3 className="text-xl font-bold">About the Job</h3>
-                    <div>
-                        <div className="mt-4">
-                            <h4 className="font-bold">Description</h4>
-                            <p className="text-sm">{selectedJob.description}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="mt-3 p-4 border rounded">
-                    <h3 className="font-bold text-xl">About the company</h3>
-
+                <section key={selectedJob.id} className={`w-full border p-4 ${id ? '' : 'hidden md:flex md:flex-col'}`}>
                     <div className="flex items-center justify-between">
                         <div className="flex items-center gap-2">
                             <Avatar>
                                 <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                                <AvatarFallback>{selectedJob.companyName} Profile Photo</AvatarFallback>
+                                <AvatarFallback>{selectedJob.companyName.charAt(0)}</AvatarFallback>
                             </Avatar>
                             <p>{selectedJob.companyName}</p>
                         </div>
-                        <Button size={'sm'} variant={'outline'}>Follow</Button>
+                        <div>
+                            <Button variant={'ghost'} size={'icon'}>
+                                <Ellipsis />
+                            </Button>
+                        </div>
                     </div>
 
-                    <p className="text-sm mt-4">{selectedJob.companyDetails}</p>
-                </div>
-            </section>
+                    <div>
+                        <h2 className="font-bold text-2xl mt-4">{selectedJob.title}</h2>
+                        <div className="flex items-center gap-2 text-[12px]">
+                            <span>{selectedJob.country}</span>
+                            <span>{timeAgo(selectedJob.createdAt)}</span>
+                            <span>{selectedJob.totalApplicants} Applicants</span>
+                        </div>
+                    </div>
+
+                    <div className="mt-4 flex flex-col gap-2">
+                        <div className="flex items-center gap-2">
+                            <BriefcaseBusinessIcon />
+                            <div className="flex items-center gap-2">
+                                {selectedJob.minSalary && <p>
+                                    <span>${selectedJob.minSalary}</span>
+                                    <span> - </span>
+                                    <span>${selectedJob.maxSalary}</span>
+                                </p>}
+                                <Badge>{selectedJob.jobType}</Badge>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <Building />
+                            <div className="flex items-center gap-2">
+                                <p>{selectedJob.employeesQuantity || '0-10'} employees</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <div>
+                                <Brain />
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <div className="flex-wrap flex gap-2">
+                                    {selectedJob.skills.split(" ").map(skill => (
+                                        <Badge variant={'secondary'} key={skill}>{skill}</Badge>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center gap-2 mt-4">
+                        <Button size={'sm'}>
+                            <MousePointerClick className="size-4 mr-1" />
+                            <span>Apply</span>
+                        </Button>
+                        <Button size={'sm'} variant={'outline'}>
+                            <Bookmark className="size-4 mr-1" />
+                            <span>Save</span>
+                        </Button>
+                    </div>
+
+                    <div className="mt-4">
+                        <h3 className="text-xl font-bold">About the Job</h3>
+                        <div>
+                            <div className="mt-4">
+                                <h4 className="font-bold">Description</h4>
+                                <p className="text-sm">{selectedJob.description}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-3 p-4 border rounded">
+                        <h3 className="font-bold text-xl">About the company</h3>
+
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-2">
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                    <AvatarFallback>{selectedJob.companyName} Profile Photo</AvatarFallback>
+                                </Avatar>
+                                <p>{selectedJob.companyName}</p>
+                            </div>
+                            <Button size={'sm'} variant={'outline'}>Follow</Button>
+                        </div>
+
+                        <p className="text-sm mt-4">{selectedJob.companyDetails}</p>
+                    </div>
+                </section>
             )}
         </div>
     );
