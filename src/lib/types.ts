1c6d70497b38832829c8dbb836974691b2b2489b
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-28 07:17:49 +0600

    feat: update job type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,22 +1,24 @@
 export interface Job {
-    id: number;
-    title: string;
-    description: string;
-    authorId: string;
-    country: string;
-    companyName: string;
-    companyDetails: string | null;
-    minSalary: number;
-    maxSalary: number;
-    totalApplicants: number;
-    skills: string;
-    url: string | null;
-    employeesQuantity: number | null;
-    jobType: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  title: string;
+  description: string;
+  authorId: string;
+  country: string;
+  companyName: string;
+  companyDetails: string | null;
+  minSalary: number;
+  maxSalary: number;
+  totalApplicants: number;
+  skills: string;
+  url: string | null;
+  employeesQuantity: number | null;
+  jobType: string;
+  experience?: string;
+  isAcceptingApplication?: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface JobCardProps {
-    job: Job;
+  job: Job;
 }
